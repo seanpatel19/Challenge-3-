fef13e430c293fc0cab96d6c4f1f45530f7eb03f
Sean Patel <seanpatel076@gmail.com>
2021-07-14 17:09:54 -0400

    Cleaned the data up by getting rid of missing values, and dollar signs. Also turned the Close column to the float data typpe

--- .ipynb_checkpoints/work done-checkpoint.py
+++ .ipynb_checkpoints/work done-checkpoint.py
@@ -9,11 +9,3 @@ bitstamp_data = pd.read_csv(
     index_col ="Date",
     infer_datetime_format = True,
     parse_dates = True)
-
-
-import pandas as pd
-import numpy as np
-
-apple_df = pd.DataFrame({"AAPL": [72.27, np.nan, 74.39, np.nan, 75.94, 76.93,np.nan, 78.74, np.nan, 79.81, np.nan, 79.53, np.nan, 79.56, 79.49]})
-
-apple_df
